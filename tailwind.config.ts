479a9f2aea3e673572aec07ed63d9c5fb7e41b5e
FlashByteOfficial <flashbyte99@gmail.com>
2024-08-07 16:17:17 +0530

    Initial commit with project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,16 @@
+import { nextui } from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ['class'],
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js"
   ],
+
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +20,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+
+  plugins: [nextui()],
 };
 export default config;
