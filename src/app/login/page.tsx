716548276cf0ad4703d831c1c2473c6ef7f895a5
FlashByteOfficial <flashbyte99@gmail.com>
2024-08-10 19:28:41 +0530

    feat(middleware): Add auth middleware for route protection, redirection
    Implement authentication middleware to safeguard private routes and redirect unauthenticated users. This middleware checks for valid session tokens and redirect users to /login route or login page  or /dashboard route or dashboard page if they are authenticated.
    
    Modify / route to redirect to /login route if user is unauthenticated and if they are authenticated redirct them to /dashboard route.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,16 @@
-export default function () {
-    return (
-        <h1>Login Page</h1>
-    )
+import { auth } from "@/lib/auth";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: `Login`,
+};
+
+export default async function () {
+  const session = await auth();
+  if (session?.user) {
+    redirect(`/dashboard`);
+  }
+
+  return <h1>Login Page</h1>;
 }
